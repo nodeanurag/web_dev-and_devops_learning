af7a82d8ad04966782c008f2245f0beba6f5f15d
nodeanurag <conveytoanurag@gmail.com>
2026-06-14 16:09:58 +0530

    purchase endpoint

--- week-08 Course Selling App {BE Project}/Day-2 Creating Backend Part_2/lecture codes/routes/course.js
+++ week-08 Course Selling App {BE Project}/Day-2 Creating Backend Part_2/lecture codes/routes/course.js
@@ -10,6 +10,49 @@ const {purchaseModel, courseModel} = require("../db");
 // Import userMiddleware to authenticate and authorize users before allowing access to routes
 const {userMiddleware} = require("../middleware/user");
 
+
+
+// Define a POST route for purchasing a course, with user authentication middleware 
+courseRouter.post("/purchase", userMiddleware, async function(req,res) {
+    // Extract userId from the request object, which was set by the userMiddleware
+    const userId = req.userId;
+
+    // Extract courseId from the request body send by the client
+    const courseId = req.body.courseId;
+
+    // If courseId is not provided in the request body, return a error response to the client
+    if(!courseId){
+        return res.status(400).json({
+            message: "Please provide a courseId",
+        });
+    }
+
+    // Check if the user has already purchased the course by quering the purchaseModel with courseId and userId
+    const existingPurchase = await purchaseModel.findOne({
+        courseId: courseId,
+        userId: userId,     
+    });
+
+    // If the user has already purchased the course, return a error response to the client
+    if(existingPurchase){
+        return res.status(400).json({
+            message:"You have already bought this course",
+        });
+    }
+
+    // Try to create a new purchase entry in the database with the provided courseId and userId
+    await purchaseModel.create({
+        courseId: courseId, // The ID of the course being purchased
+        userId: userId, // The ID of the user making the purchase
+    });
+
+    // If the purchase is successful, return a status with a success message to the client
+    res.status(201).json({
+        message: "You have successfully bought the course"
+    });
+});
+
+
 // Define the GET route for previewing course details without authentication
 courseRouter.get("/preview", async function(req,res) {
     // Query the database to get all the courses available for purchase
