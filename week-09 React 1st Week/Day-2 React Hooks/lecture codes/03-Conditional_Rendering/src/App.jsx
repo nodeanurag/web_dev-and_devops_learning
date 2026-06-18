3bbb200200c7fd9e7c22683d48fa88d97bf1ffbe
nodeanurag <conveytoanurag@gmail.com>
2026-06-18 20:29:43 +0530

    app.jsx

---
+++ week-09 React 1st Week/Day-2 React Hooks/lecture codes/03-Conditional_Rendering/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./app.css";
+
+function App() {
+  let [counterVisible, setCounterVisible] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCounterVisible((c) => !c);
+    }, 5000);
+  }, []);
+
+  return (
+    <div>
+      <h1>Conditional Rendering</h1>
+      {counterVisible && <Counter />}
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  console.log("Counter Rendered");
+
+  useEffect(() => {
+    let clock = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    console.log("Mounted");
+
+    return function () {
+      console.log("Unmounted");
+      clearInterval(clock);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+    </div>
+  );
+}
+
+export default App;
